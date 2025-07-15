8ce5981204890d3494541c25c948fef8e4d107f6
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2025-07-15 17:13:41 -0500

    Update Interfaz.py

--- Interfaz/Interfaz.py
+++ Interfaz/Interfaz.py
@@ -2,46 +2,53 @@ import tkinter as tk
 
 app = tk.Tk()
 nombre = tk.StringVar(app)
-entrada = tk.StringVar(app)
-app.geometry('600x400')#forma de la ventana(anchura x altura)
+entrada = tk.StringVar(app)  # Esta variable se usará para el Entry
+app.geometry('600x400')  # forma de la ventana(anchura x altura)
 app.configure(background='black')
 tk.Wm.wm_title(app, 'Peleas Ninja')
 
+def saludar():
+    nombre.set("Bienvido al entrenamiento ninja: " + entrada.get())
 
-#Boton
+# Campo de entrada (Entry)
+tk.Entry(
+    app,
+    font=('Courier', 14),  # Tipo de letra y tamaño
+    bg="white",
+    fg='black',
+    justify='center',
+    textvariable=entrada  # Vinculamos el Entry con la variable 'entrada'
+).pack(
+    fill=tk.BOTH,  # rellenar el botón
+    expand=True  # se expande si hay un cambio de tamaño
+)
+
+# Botón
 tk.Button(
     app,
     text="Click me",
-    font=('Courier',14),#Tipo de letra y tamano
-    bg='#00a8e8',
-    fg='white',
-    command=lambda: print('Bienvenidos al Entrenamiento Ninja' + nombre.get()),
+    font=('Courier', 14),  # Tipo de letra y tamaño
+    bg="#e5f1f6",
+    fg='black',
+    command=saludar,  # Usamos entrada.get() para capturar lo que el usuario ingresa
     relief='flat'
 ).pack(
-    fill=tk.BOTH,#rellenar el boton
-    expand=True#se expande si hay un cambio de tamano
+    fill=tk.BOTH,  # rellenar el botón
+    expand=True  # se expande si hay un cambio de tamaño
 )
 
-#imprime texto
+# Imprime texto (Label)
 tk.Label(
     app,
+    text="Inge",
     textvariable=nombre,
-    fg='white',
+    font=('Courier', 14),
+    bg='white',
+    fg='black',
     justify='center'
 ).pack(
-    fill=tk.BOTH,#rellenar el boton
-    expand=True#se expande si hay un cambio de tamano
+    fill=tk.BOTH,  # rellenar el botón
+    expand=True  # se expande si hay un cambio de tamaño
 )
 
-tk.Entry(
-    app,
-    font=('Courier',14),#Tipo de letra y tamano
-    bg="#713838",
-    fg='white',
-    justify='center'
-).pack(
-    fill=tk.BOTH,#rellenar el boton
-    expand=True#se expande si hay un cambio de tamano
-)
-
-app.mainloop()#actualiza los cambios
+app.mainloop()  # actualiza los cambios
