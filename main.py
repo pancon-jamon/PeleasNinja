4e1be89d057a485ccb14017f480bda5b116cfc81
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2025-07-22 19:04:39 -0500

    Update main.py
    se desabhilito la interfaz hasta completar el resto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from Interfaz.Interfaz import construir_interfaz
-
+from CrudAdministrador.CRUD import CrudAdministracion as administracion
+import login as ingreso
 if __name__ == "__main__":
-    construir_interfaz()
+    #construir_interfaz()
+    ingreso.ingresar()
+    # administracion.menuAdministrador()
