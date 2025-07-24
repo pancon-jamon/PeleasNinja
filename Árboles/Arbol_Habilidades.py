54ffdc56f944e9cfb68454c931a29b508ce16840
kiruncio <mistermobidic@gmail.com>
2025-07-24 18:55:52 -0500

    Árboles de habilidades -w-

--- Árboles/Arbol_Habilidades.py
+++ Árboles/Arbol_Habilidades.py
@@ -1,51 +1,104 @@
 #Árbol general de habilidades ninja
 #con posibildidad de organizarse en preorden, inorden y postorden dependiendo de cómo vaya la pelea
+#tanque, tirador, asesino
+
+"""
+Librearías:
+os: se emplea para archivos.
+random: en este caso puntual, para asignar aleatoriamente los estilos de pelea (árboles de habilidades)
+        a los peleadores.
+
+Funciones: 
+crear_archivo_habilidades(): crea y guarda los árboles binarios de habilidades de cada estilo de pelea de los ninjas, 
+                             en caso de ya existir el archivo no lo va a crear.
+"""
+
 import os
+import random
 archivo = "habilidades_ninja.txt"
 
-arbol_habilidades = {
-    "habilidad":"Llama del Dragón",
-    "izquierda":{"habilidad":"Muro de Viento Cortante",
-                 "izquierda":{"habilidad":"Escudo de Sombras",
-                              "izquierda":{"habilidad":"Curación del Fénix",
-                                           "izquierda":None,"derecha":None
-                                            },
-                              "derecha":{"habilidad":"Trampa de Niebla",
-                                         "izquierda":None,"derecha":None
-                                        }
-                  },
-                 "derecha":{"habilidad":"Esquiva Fantasma",
-                            "izquierda":{"habilidad":"Parada Perfecta",
-                                         "izquierda":None,"derecha":None                                
-                            },
-                            "derecha":{"habilidad":"Ráfaga de Garras",
-                                       "izquierda":None,"derecha":None
-                                      }                     
-                 }
+estiloTirador_habilidades = {
+    "habilidad":"Flecha de Cristal Encantada",
+    "puntos":5,
+    "izquierda":{"habilidad":"Tiro Místico",
+                 "puntos":4,
+                 "izquierda":{"habilidad":"Disparo Halcón",
+                              "puntos":2,
+                              "izquierda":None,"derecha":None                     
+                 },
+                 "derecha":None
     },
-    "derecha":{"habilidad":"Puño del Tigre Feroz",
-               "izquierda":{"habilidad":"Ataque Relámpago",
-                            "izquierda":{"habilidad":"Tornado Sigiloso",
-                                         "izquierda":None,"derecha":None
-                            },
-                            "derecha":{"habilidad":"Explosión Chakra",
-                                       "izquierda":None,"derecha":None
-                            }                   
-                },
-               "derecha":{"habilidad":"Golpe Rápido",
-                          "izquierda":None,"derecha":None                   
-               }
+    "derecha":{"habilidad":"Lluvia de Kunais",
+               "puntos":3,
+               "izquierda":None,"derecha":None
     }
 }
 
-def crear_archivo_habilidades():
+estiloAsesino_habilidades = {
+    "habilidad":"Último Suspiro",
+    "puntos":5,
+    "izquierda":{"habilidad":"Ejecución Perfecta",
+                 "puntos":4,
+                 "izquierda":None,"derecha":None
+    },
+    "derecha":{"habilidad":"Guillotina",
+               "izquierda":{"habilidad":"Golpe Rápido",
+                            "puntos":2,
+                            "izquierda":None,"derecha":None                   
+               },
+               "derecha":None
+    }
+}
+
+estiloTanque_habilidades = {
+    "habilidad":"Heraldo de la Destrucción",
+    "puntos":5,
+    "izquierda":{"habilidad":"Golpe Decisivo",
+                 "puntos":4,
+                 "izquierda":None,
+                 "derecha":{"habilidad":"Golpe Meteoro",
+                            "puntos":2,
+                            "izquierda":None,"derecha":None                     
+                 }
+    },
+    "derecha":{"habilidad":"Grito Primal",
+               "puntos":3,
+               "izquierda":None,"derecha":None
+    }
+}
+
+def crearArchivoHabilidades():
     if not os.path.exists(archivo):
-        with open("habilidades_ninja.txt", "w", encoding="utf-8") as file:
-            file.write(str(arbol_habilidades))
+        with open(archivo, "w", encoding="utf-8") as file:
+            file.write("Estilo de pelea Tirador:\n")
+            file.write(str(estiloTirador_habilidades))
+            file.write("\nEstilo de pelea Asesino:\n")
+            file.write(str(estiloAsesino_habilidades))
+            file.write("\nEstilo de pelea Tanque:\n")
+            file.write(str(estiloTanque_habilidades))
 
-crear_archivo_habilidades()
+crearArchivoHabilidades()
 
-def inorden(nodo):
+ninjas = ["Naruto","Sasuke","Sakura"]
+estilos = {"Tirador":estiloTirador_habilidades,"Asesino":estiloAsesino_habilidades,"Tanque":estiloTanque_habilidades}
+
+def asignarEstiloAleatorio(lista_ninjas,lista_estilos):
+    habilidades_asignadas = {}
+    for ninja in lista_ninjas:
+        estilo, arbol = random.choice(list(lista_estilos.items()))
+        habilidades_asignadas[ninja] = {"estilo":estilo,"arbol":arbol}
+    return habilidades_asignadas
+
+asignarEstiloAleatorio(ninjas, estilos)
+
+asignacion = asignarEstiloAleatorio(ninjas, estilos)
+for nombre, datos in asignacion.items():
+    estilo = datos["estilo"]
+    print(f"Nombre: {nombre}; Estilo: {estilo}")
+
+
+#para elegir los tipos de ataques
+def inorden(nodo): #ofensivo
     recorrido_habilidades = []
     if nodo is not None:
         recorrido_habilidades += inorden(nodo["izquierda"])
@@ -53,7 +106,7 @@ def inorden(nodo):
         recorrido_habilidades += inorden(nodo["derecha"])
     return recorrido_habilidades
 
-def preorden(nodo):
+def preorden(nodo): #equilibrado
     recorrido_habilidades = []
     if nodo is not None:
         recorrido_habilidades.append(nodo["habilidad"])
@@ -61,7 +114,7 @@ def preorden(nodo):
         recorrido_habilidades += preorden(nodo["derecha"])
     return recorrido_habilidades
 
-def postorden(nodo):
+def postorden(nodo): #defensivo
     recorrido_habilidades = []
     if nodo is not None:
         recorrido_habilidades += postorden(nodo["izquierda"])
@@ -69,28 +122,3 @@ def postorden(nodo):
         recorrido_habilidades.append(nodo["habilidad"])
     return recorrido_habilidades
 
-print("Técnica estratégica de pelea\n")
-print("1. El ninja va ganando\n2. El ninja está empatado\n3. El ninja va perdiendo")
-while True:
-    try:
-        habilidades_orden = int(input("Elija la técnica a ejecutar: "))
-        print()
-        if(habilidades_orden == 1):
-            print("Técnica ofensiva:")
-            pre_orden = preorden(arbol_habilidades)
-            print(" → ".join(pre_orden))
-            break
-        elif(habilidades_orden == 2):
-            print("Técnica de estrategia equilibrada:")
-            in_orden = inorden(arbol_habilidades)
-            print(" → ".join(in_orden))
-            break
-        elif(habilidades_orden == 3):
-            print("Técnica defensiva:")
-            in_orden = inorden(arbol_habilidades)
-            print(" → ".join(in_orden))
-            break
-        else:
-            print("Ingrese una técnica válida")
-    except ValueError:
-        print("Ingrese una técnica válida")
