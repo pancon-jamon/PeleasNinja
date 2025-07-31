0425db242ce1b054f64da54790b29601fa205007
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2025-07-30 19:44:50 -0500

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -1,17 +1,16 @@
 import os
 import estructuras
-archivo = "habilidades_ninja.txt"
+archivo_habilidades = "Archivos/habilidades_ninja.txt"
+archivo_ninjas = "Archivos/ninjas.txt"
 
 def crearArchivoUsuarios():
-    if os.path.isfile("usuarios.txt"):
-        pass
-    else:
-        with open("usuarios.txt","w"):
-            pass
+    if not os.path.exists("usuarios.txt"):
+        open("usuarios.txt", "w").close()
+        
 
 def crearArchivoHabilidades():
-    if not os.path.exists(archivo):
-        with open(archivo, "w", encoding="utf-8") as file:
+    if not os.path.exists(archivo_habilidades):
+        with open(archivo_habilidades, "w", encoding="utf-8") as file:
             file.write("Estilo de pelea Tirador:\n")
             file.write(str(estructuras.estiloTirador_habilidades))
             file.write("\nEstilo de pelea Asesino:\n")
@@ -20,8 +19,8 @@ def crearArchivoHabilidades():
             file.write(str(estructuras.estiloTanque_habilidades))
 
 def cargarNinjas():
-    if os.path.exists("ninjas.txt"):
-        with open("ninjas.txt", "r") as archivo:
+    if os.path.exists(archivo_ninjas):
+        with open(archivo_ninjas, "r") as archivo:
             for linea in archivo:
                 datos = linea.strip().split("|")
                 nombre = datos[0]
@@ -33,5 +32,5 @@ def cargarNinjas():
                 estructuras.diccionarioNinjas[nombre] = [fuerza, agilidad, resistencia, estilo, puntos]
 
 def guardarNinja(nombre, características):
-    with open("ninjas.txt", "a") as f:
+    with open(archivo_ninjas, "a") as f:
         f.write(f"{nombre}|{características[0]}|{características[1]}|{características[2]}|{características[3]}|{características[4]}\n")
